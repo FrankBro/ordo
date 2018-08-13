369f3f3521fce9369b00870f607de76740b999e5
Francois Brodeur <francois.brodeur.87@gmail.com>
2018-08-12 23:58:26 -0400

    Add back eval stuff

---
+++ Eval.fs
@@ -0,0 +1,29 @@
+module Eval
+
+open Expr
+
+let rec evalExpr env (expr: Expr) : Value =
+    match expr with
+    | EValue v -> v
+    | EVar name -> evalExpr env (EValue (Map.find name env))
+    | ECall (fnExpr, argExpr) -> evalCall env fnExpr argExpr
+    | ELet (name, valueExpr, bodyExpr) ->
+        let value = evalExpr env valueExpr
+        let env = Map.add name value env
+        evalExpr env bodyExpr
+
+and evalCall env (fnExpr: Expr) (argExpr: Expr) : Value =
+    let fnValue =
+        match fnExpr with
+        | EVar name -> Map.find name env
+        | _ -> failwith "Call first arg is not a variable"
+    match fnValue with
+    | VFun (argName, bodyExpr) ->
+        let initialEnv = env
+        let argValue = evalExpr initialEnv argExpr
+        let fnEnv = Map.add argName argValue env
+        evalExpr fnEnv bodyExpr
+    | _ -> failwith "fn_value was not a fun"
+
+let eval expr : Value =
+    evalExpr Map.empty expr
