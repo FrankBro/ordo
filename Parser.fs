31e98fad32c76df893158126f6835cb687a731f3
Francois Brodeur <francois.brodeur@adgear.com>
2018-10-22 13:37:31 -0400

    Partial parsing of types

--- Parser.fs
+++ Parser.fs
@@ -265,60 +265,92 @@ let inline readExprList input : Expr list =
     let parser = (sepEndBy parseExpr ws) .>> eof
     readOrThrow parser input
 
-// let parseTy, parseTyRef = createParserForwardedToRef ()
+let parseTy, parseTyRef = createParserForwardedToRef ()
 
-// let parseTyWs = parseTy .>> ws
-// let parseTyListWs = sepBy parseTyWs (strWs ",")
+let parseTyWs = parseTy .>> ws
+let parseTyListWs = sepBy parseTyWs (strWs ",")
 
-// let parseTConst =
-//     identWs |>> TConst
+let parseTConst =
+    identWs |>> TConst
 
-// let parseTApp : Parser<Ty> =
-//     parseTyWs .>> strWs "[" .>>. parseTyListWs .>> strWs "]"
-//     |>> TApp
+let parseTBool =
+    strWs "bool" |>> fun _ -> TBool
 
-// let parseTArrow : Parser<Ty> =
-//     parseTyWs .>> strWs "->" .>>. parseTyWs
-//     |>> TArrow
+let parseTInt =
+    strWs "int" |>> fun _ -> TInt
 
-// let nameToIdMap = ref Map.empty
+let parseTFloat =
+    strWs "float" |>> fun _ -> TFloat
 
-// let nameToId s : Ty =
-//     let m = !nameToIdMap
-//     m 
-//     |> Map.tryFind s 
-//     |> Option.defaultWith (fun () ->
-//         let id = newGenVar ()
-//         nameToIdMap := Map.add s id m
-//         id
-//     )
+let identList = sepBy1 identWs ws1
 
-// let parseTVar : Parser<Ty> =
-//     strWs "'" >>. identWs
-//     |>> nameToId
+let parseRowConstraint =
+    sepBy1 identWs (strWs "\\")
 
-// let parseTEmptyRecord : Parser<Ty> =
-//     strWs "{" .>> strWs "}"
-//     |>> fun _ -> TRecord TRowEmpty
+let parseRowConstraints =
+    sepBy1 parseRowConstraint (strWs ",")
 
-// let parseTRowFields : Parser<(string * Ty) list> =
-//     let field = identWs .>>. parseTyWs
-//     sepBy1 field (strWs ",")
+let parseTVar =
+    let p1 = strWs1 "forall" >>. identList
+    let p2 = opt (strWs "." >>. strWs "(" >>. parseRowConstraints .>> strWs ")")
+    let p3 = strWs "=>" >>. parseTyWs
+    pipe3 p1 p2 p3 (fun vars oConstraints rest ->
+        failwith "todo"
+    )
 
-// let parseTRecord : Parser<Ty> =
-//     strWs "{" >>. parseTRowFields .>> strWs "}"
-//     |>> fun fields ->
-//         let fixedFields =
-//             fields
-//             |> Map.ofList
-//             |> Map.map (fun _ -> List.singleton)
-//         TRowExtend (fixedFields, TRowEmpty)
+let parseNotLeftRecursive = choice [
+    attempt parseTBool
+    attempt parseTInt
+    attempt parseTFloat
+    attempt parseTConst
+]
 
-// let parseTExtendRecord : Parser<Ty> =
-//     strWs "{" >>. parseTRowFields .>> strWs "|" .>>. parseTyWs .>> strWs "}"
-//     |>> fun (fields, record) ->
-//         let fixedFields =
-//             fields
-//             |> Map.ofList
-//             |> Map.map (fun _ -> List.singleton)
-//         TRowExtend (fixedFields, record)
+let parseTApp : Parser<Ty> =
+    parseTyWs .>> strWs "[" .>>. parseTyListWs .>> strWs "]"
+    |>> TApp
+
+let parseTArrow : Parser<Ty> =
+    parseTyWs .>> strWs "->" .>>. parseTyWs
+    |>> TArrow
+
+let nameToIdMap = ref Map.empty
+
+let nameToId s : Ty =
+    let m = !nameToIdMap
+    m 
+    |> Map.tryFind s 
+    |> Option.defaultWith (fun () ->
+        let id = newGenVar ()
+        nameToIdMap := Map.add s id m
+        id
+    )
+
+let parseTVar : Parser<Ty> =
+    strWs "'" >>. identWs
+    |>> nameToId
+
+let parseTEmptyRecord : Parser<Ty> =
+    strWs "{" .>> strWs "}"
+    |>> fun _ -> TRecord TRowEmpty
+
+let parseTRowFields : Parser<(string * Ty) list> =
+    let field = identWs .>>. parseTyWs
+    sepBy1 field (strWs ",")
+
+let parseTRecord : Parser<Ty> =
+    strWs "{" >>. parseTRowFields .>> strWs "}"
+    |>> fun fields ->
+        let fixedFields =
+            fields
+            |> Map.ofList
+            |> Map.map (fun _ -> List.singleton)
+        TRowExtend (fixedFields, TRowEmpty)
+
+let parseTExtendRecord : Parser<Ty> =
+    strWs "{" >>. parseTRowFields .>> strWs "|" .>>. parseTyWs .>> strWs "}"
+    |>> fun (fields, record) ->
+        let fixedFields =
+            fields
+            |> Map.ofList
+            |> Map.map (fun _ -> List.singleton)
+        TRowExtend (fixedFields, record)
