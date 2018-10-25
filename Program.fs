947264608066bc8a661497d14f2f70e75e36b85d
Francois Brodeur <francois.brodeur.87@gmail.com>
2018-10-24 23:10:18 -0400

    Gardenscaping

--- Program.fs
+++ Program.fs
@@ -11,23 +11,16 @@ open Repl
 
 let test input =
     try
-        printfn "Input"
-        printfn "%s" input
+        printfn "Input    : %s" input
         let expr = readExpr input
-        printfn "Expr"
-        printfn "%O" expr
-        printfn "Raw expr"
-        printfn "%s" (stringOfExpr expr)
-        printfn "Type"
+        printfn "Expr     : %O" expr
+        printfn "Raw expr : %s" (stringOfExpr expr)
         let ty = infer expr
-        printfn "%O" ty
-        printfn "Raw type"
-        printfn "%s" (stringOfTy ty)
-        printfn "Value"
+        printfn "Type     : %O" ty
+        printfn "Raw type : %s" (stringOfTy ty)
         let value = eval expr
-        printfn "%O" value
-        printfn "Raw value"
-        printfn "%s" (stringOfValue value)
+        printfn "Value    : %O" value
+        printfn "Raw value: %s" (stringOfValue value)
     with 
     | OrdoException e ->
         printfn "Exception"
