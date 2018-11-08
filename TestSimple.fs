e7bcf43302820bd3dd47517d25459e79dbe675b2
Francois Brodeur <francois.brodeur.87@gmail.com>
2018-11-07 21:03:37 -0500

    Split tests

---
+++ TestSimple.fs
@@ -0,0 +1,488 @@
+module TestSimple
+
+open Xunit
+
+open Error
+open Expr
+open TestUtil
+
+
+[<Fact>]
+let ``Positive integer`` () =
+    test 
+        "1"
+        (POk (EInt 1))
+        (IOk "int")
+        (EOk (VInt 1))
+
+[<Fact>]
+let ``Negative integer`` () =
+    test 
+        "-1"
+        (POk (EUnOp (Negative, EInt 1)))
+        (IOk "int")
+        (EOk (VInt -1))
+
+[<Fact>]
+let ``Positive float`` () =
+    test 
+        "3.14"
+        (POk (EFloat 3.14))
+        (IOk "float")
+        (EOk (VFloat 3.14))
+
+[<Fact>]
+let ``Negative float`` () =
+    test 
+        "-3.14"
+        (POk (EUnOp (Negative, EFloat 3.14)))
+        (IOk "float")
+        (EOk (VFloat -3.14))
+
+[<Fact>]
+let ``Float that stops at dot`` () =
+    test 
+        "3."
+        (POk (EFloat 3.))
+        (IOk "float")
+        (EOk (VFloat 3.))
+
+[<Fact>]
+let ``Boolean true`` () =
+    test
+        "true"
+        (POk (EBool true))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Boolean false`` () =
+    test
+        "false"
+        (POk (EBool false))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Unbound variable`` () =
+    test
+        "a"
+        (POk (EVar "a"))
+        (IFail (g (VariableNotFound "a")))
+        (EFail (g (VariableNotFound "a")))
+
+[<Fact>]
+let ``Simple function call`` () =
+    test
+        "f x"
+        (POk (ECall (EVar "f", EVar "x")))
+        (IFail (g (VariableNotFound "f")))
+        (EFail (g (VariableNotFound "f")))
+
+[<Fact>]
+let ``Function call with multiple args`` () =
+    test
+        "f x y"
+        (POk (ECall (ECall (EVar "f", EVar "x"), EVar "y")))
+        (IFail (g (VariableNotFound "f")))
+        (EFail (g (VariableNotFound "f")))
+
+[<Fact>]
+let ``Simple let`` () =
+    test
+        "let a = 1 in a"
+        (POk (ELet (EVar "a", EInt 1, EVar "a")))
+        (IOk "int")
+        (EOk (VInt 1))
+
+[<Fact>]
+let ``Let call function`` () =
+    test
+        "let f = fun a -> a in f 1"
+        (POk (ELet (EVar "f", EFun (EVar "a", EVar "a"), ECall (EVar "f", EInt 1))))
+        (IOk "int")
+        (EOk (VInt 1))
+
+[<Fact>]
+let ``Empty record`` () =
+    test
+        "{}"
+        (POk (eRecord []))
+        (IOk "{}")
+        (EOk (vRecord []))
+
+[<Fact>]
+let ``Record restrict`` () =
+    test
+        "let r = { a = 1 } in r\\a"
+        (POk (ELet (EVar "r", eRecord ["a", EInt 1], ERecordRestrict (EVar "r", "a"))))
+        (IOk "{}")
+        (EOk (vRecord []))
+
+[<Fact>]
+let ``Record extend`` () =
+    test
+        "let r = { a = 1 } in { b = 2 | r }"
+        (POk (ELet (EVar "r", eRecord ["a", EInt 1], ERecordExtend ("b", EInt 2, EVar "r"))))
+        (IOk "{a : int, b : int}")
+        (EOk (vRecord ["a", VInt 1; "b", VInt 2]))
+
+[<Fact>]
+let ``Record select`` () =
+    test
+        "let r = { a = 1 } in r.a"
+        (POk (ELet (EVar "r", eRecord ["a", EInt 1], ERecordSelect (EVar "r", "a"))))
+        (IOk "int")
+        (EOk (VInt 1))
+
+[<Fact>]
+let ``Variant`` () =
+    test
+        ":a 1"
+        (POk (EVariant ("a", EInt 1)))
+        (IOk "forall r. (r\\a) => <a : int | r>")
+        (EOk (VVariant ("a", VInt 1)))
+
+[<Fact>]
+let ``If then else`` () =
+    test
+        "if true then 1 else 0"
+        (POk (EIfThenElse (EBool true, EInt 1, EInt 0)))
+        (IOk "int")
+        (EOk (VInt 1))
+
+[<Fact>]
+let ``If value must be bool`` () =
+    test
+        "if 1 then 1 else 0"
+        (POk (EIfThenElse (EInt 1, EInt 1, EInt 0)))
+        (IFail (i (UnifyFail (TInt, TBool))))
+        (EFail (g IfValueNotBoolean))
+
+[<Fact>]
+let ``Plus integer`` () =
+    test
+        "1 + 2"
+        (POk (EBinOp (EInt 1, Plus, EInt 2)))
+        (IOk "int")
+        (EOk (VInt 3))
+
+[<Fact>]
+let ``Plus float`` () =
+    test
+        "1. + 2."
+        (POk (EBinOp (EFloat 1., Plus, EFloat 2.)))
+        (IOk "float")
+        (EOk (VFloat 3.))
+
+[<Fact>]
+let ``Binop fail`` () =
+    test
+        "1 + 2."
+        (POk (EBinOp (EInt 1, Plus, EFloat 2.)))
+        (IFail (i (UnifyFail (TInt, TFloat))))
+        (EFail (e BadBinOp))
+
+[<Fact>]
+let ``Minus integer`` () =
+    test
+        "3 - 2"
+        (POk (EBinOp (EInt 3, Minus, EInt 2)))
+        (IOk "int")
+        (EOk (VInt 1))
+
+[<Fact>]
+let ``Minus float`` () =
+    test
+        "3. - 2."
+        (POk (EBinOp (EFloat 3., Minus, EFloat 2.)))
+        (IOk "float")
+        (EOk (VFloat 1.))
+
+[<Fact>]
+let ``Multiply integer`` () =
+    test
+        "3 * 2"
+        (POk (EBinOp (EInt 3, Multiply, EInt 2)))
+        (IOk "int")
+        (EOk (VInt 6))
+
+[<Fact>]
+let ``Multiply float`` () =
+    test
+        "3. * 2."
+        (POk (EBinOp (EFloat 3., Multiply, EFloat 2.)))
+        (IOk "float")
+        (EOk (VFloat 6.))
+
+[<Fact>]
+let ``Divide integer`` () =
+    test
+        "6 / 2"
+        (POk (EBinOp (EInt 6, Divide, EInt 2)))
+        (IOk "int")
+        (EOk (VInt 3))
+
+[<Fact>]
+let ``Divide float`` () =
+    test
+        "6. / 2."
+        (POk (EBinOp (EFloat 6., Divide, EFloat 2.)))
+        (IOk "float")
+        (EOk (VFloat 3.))
+    
+[<Fact>]
+let ``And true`` () =
+    test
+        "true && true"
+        (POk (EBinOp (EBool true, And, EBool true)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``And false`` () =
+    test
+        "true && false"
+        (POk (EBinOp (EBool true, And, EBool false)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Or true`` () =
+    test
+        "true || false"
+        (POk (EBinOp (EBool true, Or, EBool false)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Or false`` () =
+    test
+        "false || false"
+        (POk (EBinOp (EBool false, Or, EBool false)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Integer Equal true`` () =
+    test
+        "1 = 1"
+        (POk (EBinOp (EInt 1, Equal, EInt 1)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Integer Equal false`` () =
+    test
+        "1 = 2"
+        (POk (EBinOp (EInt 1, Equal, EInt 2)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Float Equal true`` () =
+    test
+        "1. = 1."
+        (POk (EBinOp (EFloat 1., Equal, EFloat 1.)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Float Equal false`` () =
+    test
+        "1. = 2."
+        (POk (EBinOp (EFloat 1., Equal, EFloat 2.)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Bool Equal true`` () =
+    test
+        "true = true"
+        (POk (EBinOp (EBool true, Equal, EBool true)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Bool Equal false`` () =
+    test
+        "true = false"
+        (POk (EBinOp (EBool true, Equal, EBool false)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Integer NotEqual true`` () =
+    test
+        "1 <> 2"
+        (POk (EBinOp (EInt 1, NotEqual, EInt 2)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Integer NotEqual false`` () =
+    test
+        "1 <> 1"
+        (POk (EBinOp (EInt 1, NotEqual, EInt 1)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Float NotEqual true`` () =
+    test
+        "1. <> 2."
+        (POk (EBinOp (EFloat 1., NotEqual, EFloat 2.)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Float NotEqual false`` () =
+    test
+        "1. <> 1."
+        (POk (EBinOp (EFloat 1., NotEqual, EFloat 1.)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Bool NotEqual true`` () =
+    test
+        "true <> false"
+        (POk (EBinOp (EBool true, NotEqual, EBool false)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Bool NotEqual false`` () =
+    test
+        "true <> true"
+        (POk (EBinOp (EBool true, NotEqual, EBool true)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Integer Greater true`` () =
+    test
+        "2 > 1"
+        (POk (EBinOp (EInt 2, Greater, EInt 1)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Integer Greater false`` () =
+    test
+        "1 > 2"
+        (POk (EBinOp (EInt 1, Greater, EInt 2)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Float Greater true`` () =
+    test
+        "2. > 1."
+        (POk (EBinOp (EFloat 2., Greater, EFloat 1.)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Float Greater false`` () =
+    test
+        "1. > 2."
+        (POk (EBinOp (EFloat 1., Greater, EFloat 2.)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Integer GreaterEqual true`` () =
+    test
+        "2 >= 2"
+        (POk (EBinOp (EInt 2, GreaterEqual, EInt 2)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Integer GreaterEqual false`` () =
+    test
+        "1 >= 2"
+        (POk (EBinOp (EInt 1, GreaterEqual, EInt 2)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Float GreaterEqual true`` () =
+    test
+        "2. >= 2."
+        (POk (EBinOp (EFloat 2., GreaterEqual, EFloat 2.)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Float GreaterEqual false`` () =
+    test
+        "1. >= 2."
+        (POk (EBinOp (EFloat 1., GreaterEqual, EFloat 2.)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Integer Lesser true`` () =
+    test
+        "2 < 3"
+        (POk (EBinOp (EInt 2, Lesser, EInt 3)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Integer Lesser false`` () =
+    test
+        "3 < 2"
+        (POk (EBinOp (EInt 3, Lesser, EInt 2)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Float Lesser true`` () =
+    test
+        "2. < 3."
+        (POk (EBinOp (EFloat 2., Lesser, EFloat 3.)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Float Lesser false`` () =
+    test
+        "3. < 2."
+        (POk (EBinOp (EFloat 3., Lesser, EFloat 2.)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Integer LesserEqual true`` () =
+    test
+        "2 <= 2"
+        (POk (EBinOp (EInt 2, LesserEqual, EInt 2)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Integer LesserEqual false`` () =
+    test
+        "3 <= 2"
+        (POk (EBinOp (EInt 3, LesserEqual, EInt 2)))
+        (IOk "bool")
+        (EOk (VBool false))
+
+[<Fact>]
+let ``Float LesserEqual true`` () =
+    test
+        "2. <= 2."
+        (POk (EBinOp (EFloat 2., LesserEqual, EFloat 2.)))
+        (IOk "bool")
+        (EOk (VBool true))
+
+[<Fact>]
+let ``Float LesserEqual false`` () =
+    test
+        "3. <= 2."
+        (POk (EBinOp (EFloat 3., LesserEqual, EFloat 2.)))
+        (IOk "bool")
+        (EOk (VBool false))
