36bc6970332567d3da85112195dd8cb2bfa81e11
Francois Brodeur <francois.brodeur.87@gmail.com>
2019-01-12 23:04:38 -0500

    Transform inner guards

--- Transform.fs
+++ Transform.fs
@@ -3,6 +3,16 @@ module Transform
 open Error
 open Expr
 
+// Before
+// let { a = 1 } = { a = 1 } in 1
+// After
+// let _var0 = { a = 1 } in let _var1 = _var0.a in if a = 1 then 1 else error "bad match"
+
+// Before
+// match a { :a 1 -> 1 }
+// After
+//
+
 let currentId = ref 0
 
 let nextId () =
@@ -16,6 +26,10 @@ let getNewVar () =
 
 let rec transformRowBinding var body expr =
     match expr with
+    | EBool b -> failwith ""
+    | EInt i -> failwith ""
+    | EFloat f -> failwith ""
+    | EString s -> failwith ""
     | EVar _  -> body
     | EVariant (_, EVar _) -> 
         ECase (EVar var, [expr, body, None], None)
@@ -34,8 +48,45 @@ let rec transformRowBinding var body expr =
     | ERecordEmpty -> body
     | _ -> raise (genericError (InvalidPattern expr))
 
-let rec transformRowMatch var body expr =
-    failwith ""
+let isValueType expr =
+    match expr with
+    | EBool _
+    | EInt _
+    | EFloat _ 
+    | EString _ -> true
+    | _ -> false
+
+let extractValueGuard var expr =
+    match expr with
+    | EBool _
+    | EInt _
+    | EFloat _
+    | EString _ -> EBinOp (EVar var, BinOp.Equal, expr)
+    | _ -> raise (genericError (InvalidPattern expr))
+
+let rec extractGuards pattern guards =
+    match pattern with
+    | EVar _  -> pattern, guards
+    | EVariant (_, EVar _) -> pattern, guards
+    | EVariant (case, body) when isValueType body -> 
+        let var = getNewVar ()
+        let guard = extractValueGuard var body
+        EVariant (case, EVar var), guard :: guards
+    | EVariant (case, body) ->
+        let body, guards = extractGuards body guards
+        EVariant (case, body), guards
+    | ERecordExtend (_, EVar _, record) -> extractGuards record guards
+    | ERecordExtend (name, body, record) when isValueType body ->
+        let var = getNewVar ()
+        let record, guards = extractGuards record guards
+        let guard = extractValueGuard var body
+        ERecordExtend (name, EVar var, record), guard :: guards
+    | ERecordExtend (name, body, record) ->
+        let body, guards = extractGuards body guards
+        let record, guards = extractGuards record guards
+        ERecordExtend (name, body, record), guards
+    | ERecordEmpty -> pattern, guards
+    | _ -> raise (genericError (InvalidPattern pattern))
 
 let rec transformExpr expr =
     match expr with
@@ -56,12 +107,22 @@ let rec transformExpr expr =
         let fixedCases = 
             cases
             |> List.map (fun (pattern, body, oGuard) ->
-                let pattern, guards = transformRowMatch a b c
-                let fullGuards =
+                let guards =
                     match oGuard with
-                    | None -> guards
-                    | Some previousGuards -> EBinOp (guards, BinOp.And, previousGuards)
-                pattern, body, Some fullGuards
+                    | None -> []
+                    | Some previousGuards -> [previousGuards]
+                let pattern, guards = extractGuards pattern guards
+                let oGuard =
+                    match guards with
+                    | [] -> None
+                    | guard :: [] -> Some guard
+                    | guard :: guards ->
+                        (guard, guards)
+                        ||> List.fold (fun state guard ->
+                            EBinOp (state, BinOp.And, guard)
+                        )
+                        |> Some
+                pattern, body, oGuard
             )
         ECase (value, fixedCases, oDefault)
     | _ -> expr
