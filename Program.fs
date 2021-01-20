6ca9a2f314635132eaf52ed8d85384d79ea2f3fb
Francois Brodeur <francois.brodeur.87@gmail.com>
2021-01-19 22:09:43 -0500

    Fuck emitting lua, upgrade everything

--- Program.fs
+++ Program.fs
@@ -4,23 +4,14 @@ open System
 open System.Diagnostics
 open System.IO
 
-open Compiler
 open Error
 open Expr
 open Infer
 open Parse
 open Repl
 open Util
-open TestEmitter
 
 [<EntryPoint>]
 let main argv =
-    [
-        "ffi"
-        "expr"
-    ]
-    |> compile "bootstrap"
-
-    System.Console.ReadKey () |> ignore
-
+    runRepl ()
     0 // return an integer exit code
